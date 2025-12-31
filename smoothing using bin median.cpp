e692f223e906b1bcc0920d99e7934969e53c8f51
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2025-12-31 23:27:36 +0530

    Create smoothing using bin median.cpp

---
+++ smoothing using bin median.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int bins,n;
+    double sum = 0.0, median = 0.0;
+    cout << "no. of bins = " <<endl;
+    cin >> bins;
+    cout << "no. of eles per bins = " <<endl;
+    cin >> n;
+    vector<int> a(bins*n);
+    for(int i=0;i<bins*n;i++) cin >> a[i];
+    sort(a.begin(), a.end(), greater<int>());
+    cout << "given eles are\n";
+    for(int i=0;i<bins;i++) {
+        for(int j=0;j<n;j++) cout << a[i*n+j] << " ";
+        cout <<endl;
+    }
+    cout << "mean vals are\n";
+    for(int i=0;i<bins;i++) {
+        median = a[i*n+n/2];
+        for(int j=0;j<n;j++) cout << median << " ";
+        cout <<endl;
+    }
+}
