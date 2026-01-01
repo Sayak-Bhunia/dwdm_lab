08829432987f53cafaf0899cb3235bf5f28161b4
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2026-01-01 13:38:57 +0530

    Update nearest neighbour classification.cpp

--- nearest neighbour classification.cpp
+++ nearest neighbour classification.cpp
@@ -1,71 +1,86 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-static int row, col, min_freq = 2;
+int tuples, attr;
+float nearv[20];
 
-void findFrequentSets(int items[][100], int transactions[][100],
-                      int no_of_items[], int no_of_transactions,
-                      int result_item[][100]);
-
-void generateSubsets(int items[][100], int setCount, int result_item[][100]);
+void NNC(int d[20][20], int t[1][20]);
+int isNear();
 
 int main()
 {
-    int no_of_transactions, s = 0, k = 1, l = 0, i, j, setCount = 2, maxCount;
-    int transactions[100][100], no_of_items[100], items[100][100];
-    int result_item[100][100];
+    int i, j, k, data[20][20], test[1][20], s;
 
-    cout << "enter no.of transactions\n";
-    cin >> no_of_transactions;
+    cout << "enter no of tuples:";
+    cin >> tuples;
 
-    for (i = 1; i <= no_of_transactions; i++)
+    cout << "enter no of attributes:";
+    cin >> attr;
+
+    cout << "enter " << tuples << " tuples\n";
+    for (i = 0; i < tuples; i++)
     {
-        cout << "enter no.of items in transaction:" << i << "\n";
-        cin >> no_of_items[i];
-        cout << "enter " << no_of_items[i] << " items for transaction:" << i << "\n";
-        for (j = 1; j <= no_of_items[i]; j++)
-            cin >> transactions[i][j];
-    }
-
-    k = 0;
-    for (i = 1; i <= no_of_transactions; i++)
-    {
-        for (j = 1; j <= no_of_items[i]; j++)
+        for (j = 0; j < attr; j++)
         {
-            for (l = 1; l <= k; l++)
-                if (items[l][1] == transactions[i][j])
-                    break;
-            if (l > k)
-                items[++k][1] = transactions[i][j];
+            cin >> data[i][j];
         }
     }
 
-    row = k;
-    col = 1;
-    maxCount = k;
-
-    findFrequentSets(items, transactions, no_of_items, no_of_transactions, result_item);
-    cout << "\n";
-
-    for (i = 1; i <= row; i++)
+    cout << "enter test tuple\n";
+    for (i = 0; i < attr; i++)
     {
-        for (j = 1; j <= col; j++)
-            cout << result_item[i][j] << " ";
-        cout << "\n";
+        cin >> test[0][i];
     }
 
-    while (setCount <= maxCount)
-    {
-        generateSubsets(result_item, setCount, items);
-        findFrequentSets(items, transactions, no_of_items, no_of_transactions, result_item);
+    cout << "enter k value:";
+    cin >> k;
 
-        for (i = 1; i <= row; i++)
+    NNC(data, test);
+
+    cout << "The nearest neighbours are\n";
+    for (i = 0; i < k; i++)
+    {
+        s = isNear();
+        cout << "(";
+        for (j = 0; j < attr; j++)
         {
-            for (j = 1; j <= col; j++)
-                cout << result_item[i][j] << " ";
-            cout << "\n";
+            cout << data[s][j];
         }
-        setCount++;
+        cout << ")\n";
     }
+
     return 0;
+}
+
+void NNC(int d[20][20], int t[1][20])
+{
+    int i, j, sum = 0;
+
+    for (i = 0; i < tuples; i++)
+    {
+        for (j = 0; j < attr; j++)
+        {
+            sum += (d[i][j] - t[0][j]) * (d[i][j] - t[0][j]);
+        }
+        nearv[i] = sqrt(sum);
+        sum = 0;
+    }
+}
+
+int isNear()
+{
+    float t = nearv[0];
+    int s = 0, i;
+
+    for (i = 1; i < tuples; i++)
+    {
+        if (t >= nearv[i] && nearv[i] != 999)
+        {
+            t = nearv[i];
+            s = i;
+        }
+    }
+    nearv[s] = 999;
+    return s;
 }
