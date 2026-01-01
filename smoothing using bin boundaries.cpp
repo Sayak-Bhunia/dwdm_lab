c9d328c5832bb1830a7268e1cb0737afc9615478
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2026-01-01 09:47:09 +0530

    Update smoothing using bin boundaries.cpp

--- smoothing using bin boundaries.cpp
+++ smoothing using bin boundaries.cpp
@@ -1,24 +1,29 @@
-#include<iostream>
-#include<bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main() {
-    int bins,n;
-    double sum = 0.0, median = 0.0;
-    cout << "no. of bins = " <<endl;
+    int bins, n;
+    cout << "enter no. of bins = " <<endl;
     cin >> bins;
-    cout << "no. of eles per bins = " <<endl;
+    cout << "enter no. of eles per bin = " <<endl;
     cin >> n;
     vector<int> a(bins*n);
+    cout << "enter elements = " <<endl;
     for(int i=0;i<bins*n;i++) cin >> a[i];
-    sort(a.begin(), a.end(), greater<int>());
-    cout << "mean vals are\n";
+    cout << "given elements = " <<endl;
+    for(int i=0;i<bins;i++) {
+        for(int j=0;j<n;j++) cout << a[i*n+j] << "\t";
+        cout <<endl;
+    }
+    cout << "bin boundaries are = " <<endl;
     for(int i=0;i<bins;i++) {
         int l = a[i*n], h = a[i*n+n-1];
         for(int j=0;j<n;j++) {
             int val = a[i*n+j];
-            if(abs(val-l)<=val(val-h)) cout << l << " ";
-            else cout << h << " ";
+            if(abs(val-l)<=abs(val-h)) cout << l << "\t";
+            else cout << h << "\t";
         }
         cout <<endl;
     }
