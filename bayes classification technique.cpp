eb150d2160c25df2011f76cee73255b31e88a1ce
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2026-01-01 17:33:44 +0530

    Update bayes classification technique.cpp

--- bayes classification technique.cpp
+++ bayes classification technique.cpp
@@ -2,18 +2,16 @@
 #include <cstring>
 using namespace std;
 
-void classify(int n, int tuples);
+void findClass(int, int);
 
-// Global variables (as in lab manuals)
-char cls[10][20], titems[50][20][20], attr[10][20];
-int pcount[20], countc[10], fc = 0;
+char cls[10][20], items[50][20][20], attr[10][20];
+char tup[15][20];
+int pcount[20], countCls[10], fc = 0;
 float p[10], prob[50], pre[10], result[10];
 
 int main()
 {
-    char tup[15][20];
-    int i, j, n, tuples, k, ans = 0;
-    int c = 0;
+    int i, j, n, tuples, k, ans = 0, c = 0;
 
     cout << "enter no of attributes:";
     cin >> n;
@@ -21,7 +19,7 @@ int main()
     cout << "enter no of tuples:";
     cin >> tuples;
 
-    cout << "enter " << n << " attributes\n";
+    cout << "enter " << n << " attributes" << endl;
     for (i = 0; i < n; i++)
         cin >> attr[i];
 
@@ -29,56 +27,49 @@ int main()
     {
         cout << "enter tuple" << i + 1 << endl;
         for (j = 0; j < n; j++)
-            cin >> titems[i][j];
+            cin >> items[i][j];
     }
 
-    cout << "enter test tuple\n";
+    cout << "enter test tuple" << endl;
     for (i = 0; i < n - 1; i++)
         cin >> tup[i];
 
-    // Find classes and their counts
-    classify(n, tuples);
+    findClass(n, tuples);
 
-    // Prior probability P(class)
+    /* Prior probabilities */
     for (i = 0; i < fc; i++)
-        p[i] = countc[i] / (float)tuples;
+        p[i] = countCls[i] / (float)tuples;
 
-    // Conditional probability P(attribute | class)
+    /* Conditional probabilities */
     for (i = 0; i < fc; i++)
     {
         for (j = 0; j < n - 1; j++)
         {
             pcount[j] = 0;
-
             for (k = 0; k < tuples; k++)
             {
-                if (strcmp(titems[k][j], tup[j]) == 0 &&
-                    strcmp(titems[k][n - 1], cls[i]) == 0)
-                {
+                if (strcmp(items[k][j], tup[j]) == 0 &&
+                    strcmp(cls[i], items[k][n - 1]) == 0)
                     pcount[j]++;
-                }
             }
 
-            // Laplace smoothing
-            if (pcount[j] != 0)
-                prob[c++] = pcount[j] / (float)countc[i];
+            /* Laplace smoothing */
+            if (pcount[j] == 0)
+                prob[c++] = (pcount[j] + 1) / (float)countCls[i];
             else
-                prob[c++] = (pcount[j] + 1) / (float)countc[i];
+                prob[c++] = pcount[j] / (float)countCls[i];
         }
     }
 
-    // Multiply probabilities correctly
-    int idx = 0;
-    int attrs = n - 1;
-
+    /* Posterior probabilities */
+    c = 0;
     for (i = 0; i < fc; i++)
     {
         pre[i] = 1.0;
-        for (j = 0; j < attrs; j++)
-            pre[i] *= prob[idx++];
+        for (j = 0; j < n - 1; j++)
+            pre[i] *= prob[c++];
     }
 
-    // Final probability
     for (i = 0; i < fc; i++)
     {
         result[i] = pre[i] * p[i];
@@ -86,40 +77,167 @@ int main()
             ans = i;
     }
 
-    cout << "The test tuple belongs to " << cls[ans] << " class\n";
-
+    cout << "The test tuple belongs to " << cls[ans] << " class" << endl;
     return 0;
 }
 
-// Function to find unique classes and count
-void classify(int p, int q)
+void findClass(int p, int q)
 {
     int i, k, t;
 
-    strcpy(cls[fc++], titems[0][p - 1]);
+    strcpy(cls[fc++], items[0][p - 1]);
 
     for (i = 1; i < q; i++)
     {
         t = 0;
         for (k = 0; k < fc; k++)
         {
-            if (strcmp(titems[i][p - 1], cls[k]) == 0)
+            if (strcmp(items[i][p - 1], cls[k]) == 0)
             {
                 t = -1;
                 break;
             }
         }
         if (t != -1)
-            strcpy(cls[fc++], titems[i][p - 1]);
+            strcpy(cls[fc++], items[i][p - 1]);
     }
 
     for (i = 0; i < fc; i++)
     {
-        countc[i] = 0;
+        countCls[i] = 0;
         for (k = 0; k < q; k++)
         {
-            if (strcmp(titems[k][p - 1], cls[i]) == 0)
-                countc[i]++;
+            if (strcmp(items[k][p - 1], cls[i]) == 0)
+                countCls[i]++;
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include <stdio.h>
+#include <string.h>
+
+void class(int, int);
+
+char cls[10][20], items[50][20][20], attr[10][20];
+char tup[15][20];
+int pcount[20], count[10], fc = 0;
+float p[10], prob[50], pre[10], result[10];
+
+int main()
+{
+    int i, j, n, tuples, k, ans = 0, c = 0;
+
+    printf("enter no of attributes:");
+    scanf("%d", &n);
+
+    printf("enter no of tuples:");
+    scanf("%d", &tuples);
+
+    printf("enter %d attributes\n", n);
+    for (i = 0; i < n; i++)
+        scanf("%s", attr[i]);
+
+    for (i = 0; i < tuples; i++)
+    {
+        printf("enter tuple%d\n", i + 1);
+        for (j = 0; j < n; j++)
+            scanf("%s", items[i][j]);
+    }
+
+    printf("enter test tuple\n");
+    for (i = 0; i < n - 1; i++)
+        scanf("%s", tup[i]);
+
+    class(n, tuples);
+
+    /* Prior probabilities */
+    for (i = 0; i < fc; i++)
+        p[i] = count[i] / (float)tuples;
+
+    /* Conditional probabilities */
+    for (i = 0; i < fc; i++)
+    {
+        for (j = 0; j < n - 1; j++)   // FIXED index
+        {
+            pcount[j] = 0;
+            for (k = 0; k < tuples; k++)
+            {
+                if (strcmp(items[k][j], tup[j]) == 0 &&
+                    strcmp(cls[i], items[k][n - 1]) == 0)
+                    pcount[j]++;
+            }
+
+            /* Laplace smoothing */
+            if (pcount[j] == 0)
+                prob[c++] = (pcount[j] + 1) / (float)count[i];
+            else
+                prob[c++] = pcount[j] / (float)count[i];
+        }
+    }
+
+    /* Posterior probabilities */
+    c = 0;
+    for (i = 0; i < fc; i++)
+    {
+        pre[i] = 1.0;
+        for (j = 0; j < n - 1; j++)
+            pre[i] *= prob[c++];
+    }
+
+    for (i = 0; i < fc; i++)
+    {
+        result[i] = pre[i] * p[i];
+        if (i > 0 && result[i] > result[ans])
+            ans = i;
+    }
+
+    printf("The test tuple belongs to %s class\n", cls[ans]);
+    return 0;
+}
+
+/* Find unique classes and counts */
+void class(int p, int q)
+{
+    int i, k, t;
+
+    strcpy(cls[fc++], items[0][p - 1]);
+
+    for (i = 1; i < q; i++)
+    {
+        t = 0;
+        for (k = 0; k < fc; k++)
+        {
+            if (strcmp(items[i][p - 1], cls[k]) == 0)
+            {
+                t = -1;
+                break;
+            }
+        }
+        if (t != -1)
+            strcpy(cls[fc++], items[i][p - 1]);
+    }
+
+    for (i = 0; i < fc; i++)
+    {
+        count[i] = 0;
+        for (k = 0; k < q; k++)
+        {
+            if (strcmp(items[k][p - 1], cls[i]) == 0)
+                count[i]++;
+        }
+    }
+}
+
