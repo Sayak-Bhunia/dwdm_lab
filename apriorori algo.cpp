6d50eddf9058017631b62f45ce312fccfe631507
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2026-01-01 13:40:06 +0530

    Update apriorori algo.cpp

--- apriorori algo.cpp
+++ apriorori algo.cpp
@@ -69,3 +69,76 @@ int main()
     }
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include<stdio.h>
+
+static int row, col, min_freq = 2;
+
+void findFrequentSets(int items[][100], int transactions[][100],
+                      int no_of_items[], int no_of_transactions,
+                      int result_item[][100]);
+
+void generateSubsets(int items[][100], int setCount, int result_item[][100]);
+
+void main()
+{
+    int no_of_transactions, s = 0, k = 1, l = 0, i, j, setCount = 2, maxCount;
+    int transactions[100][100], no_of_items[100], items[100][100];
+    int result_item[100][100];
+
+    printf("enter no.of transactions\n");
+    scanf("%d", &no_of_transactions);
+
+    for (i = 1; i <= no_of_transactions; i++)
+    {
+        printf("enter no.of items in transaction:%d\n", i);
+        scanf("%d", &no_of_items[i]);
+        printf("enter %d items for transaction:%d\n", no_of_items[i], i);
+        for (j = 1; j <= no_of_items[i]; j++)
+            scanf("%d", &transactions[i][j]);
+    }
+
+    k = 0;
+    for (i = 1; i <= no_of_transactions; i++)
+    {
+        for (j = 1; j <= no_of_items[i]; j++)
+        {
+            for (l = 1; l <= k; l++)
+                if (items[l][1] == transactions[i][j])
+                    break;
+            if (l > k)
+                items[++k][1] = transactions[i][j];
+        }
+    }
+
+    row = k;
+    col = 1;
+    maxCount = k;
+
+    findFrequentSets(items, transactions, no_of_items, no_of_transactions, result_item);
+
+    for (i = 1; i <= row; i++)
+    {
+        for (j = 1; j <= col; j++)
+            printf("%d ", result_item[i][j]);
+        printf("\n");
+    }
+}
+`
+
