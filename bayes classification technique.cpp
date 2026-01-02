59a9ae4f17e7b089ab5cc1eb0191a67facb9e1d4
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2026-01-02 23:48:13 +0530

    Update bayes classification technique.cpp

--- bayes classification technique.cpp
+++ bayes classification technique.cpp
@@ -241,3 +241,92 @@ void class(int p, int q)
     }
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+char cls[10][20];
+int countCls[10], fc = 0;
+
+void findClass(char items[][20][20], int n, int tuples) {
+    strcpy(cls[fc++], items[0][n-1]);
+    for(int i=1;i<tuples;i++) {
+        bool f = false;
+        for(int j=0;j<fc;j++) {
+            if(strcmp(items[i][n-1], cls[j]) == 0) {
+                f = true;
+                break;
+            }
+        }
+        if(!f) strcpy(cls[fc++], items[i][n-1]);
+    }
+    for(int i=0;i<fc;i++) {
+        countCls[i] = 0;
+        for(int j=0;j<tuples;j++) {
+            if(strcmp(items[j][n-1], cls[i]) == 0) countCls[i]++;
+        }
+    } 
+}
+
+int main() {
+    int j, k, n, tuples, res = 0, pc[20], c = 0;
+    char attributes[10][20], items[50][20][20], tup[15][20];
+    float p[10], prob[50], pre[10], result[10];
+    cout << "enter no of attributes = ";
+    cin >> n;
+    cout << "enter no of tuples = ";
+    cin >> tuples;
+    cout << "enter" << n << " attributes" <<endl;
+    for(int i=0;i<n;i++) cin >> attributes[i];
+    for(int i=0;i<tuples;i++) {
+        cout << "enter tuple" << i+1 <<endl;
+        for(int j=0;j<n;j++) cin >> items[i][j];
+    }
+    cout << "enter test tuple = " <<endl;
+    for(int i=0;i<n-1;i++) cin >> tup[i];
+    findClass(items, n, tuples);
+    for(int i=0;i<fc;i++) p[i] = countCls[i]/(float)tuples;
+    for(int i=0;i<fc;i++) {
+        for(int j=0;j<n-1;j++) {
+            pc[j] = 0;
+            for(int k=0;k<tuples;k++) {
+                if(strcmp(items[k][j], tup[j]) == 0 && strcmp(items[k][n-1], cls[i]) == 0) pc[j]++;
+            }
+            prob[c++] = (pc[j] == 0)?(1.0/countCls[i]):(pc[j]/(float)countCls[i]);
+        }
+    }
+    c = 0;
+    for(int i=0;i<fc;i++) {
+        pre[i] = 1.0;
+        for(int j=0;j<n-1;j++) pre[i] *= prob[c++];
+    }
+    for(int i=0;i<fc;i++) {
+        result[i] = pre[i]*p[i];
+        if(i>0 && result[i]>result[res]) res = i;
+    }
+    cout << "test tuple belongs to = " << cls[res] << " class" <<endl;
+}
