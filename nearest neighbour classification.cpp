1588d6e37325eb70918bd7f799ba47496caf8c8e
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2026-01-03 08:58:01 +0530

    Update nearest neighbour classification.cpp

--- nearest neighbour classification.cpp
+++ nearest neighbour classification.cpp
@@ -1,90 +1,42 @@
-#include <iostream>
-#include <cmath>
+#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-int tuples, attr, k;
-float nearDist[20];
-
-void NNC(int d[20][20], int t[1][20]);
-int isNear();
-
-int main()
-{
-    int i, j, data[20][20], test[1][20], s;
-
-    cout << "enter no of tuples:";
+int main() {
+    int tuples, attributes, k, data[20][20], test[20];
+    float nearDst[20];
+    cout << "enter no. of tuples = ";
     cin >> tuples;
-
-    cout << "enter no of attributes:";
-    cin >> attr;
-
-    cout << "enter " << tuples << " tuples" << endl;
-    for (i = 0; i < tuples; i++)
-    {
-        for (j = 0; j < attr; j++)
-        {
-            cin >> data[i][j];
-        }
+    cout << "enter no. of attributes = ";
+    cin >> attributes;
+    cout << "enter " << tuples << " tuples" <<endl;
+    for(int i=0;i<tuples;i++) {
+        for(int j=0;j<attributes;j++) cin >> data[i][j];
     }
-
-    cout << "enter test tuple" << endl;
-    for (i = 0; i < attr; i++)
-    {
-        cin >> test[0][i];
-    }
-
-    cout << "enter k value:";
+    cout << "enter test tuple = " <<endl;
+    for(int j=0;j<attributes;j++) cin >> test[j];
+    cout << "enter k value = ";
     cin >> k;
-
-    NNC(data, test);
-
-    cout << "The nearest neighbours are" << endl;
-    for (i = 0; i < k; i++)
-    {
-        s = isNear();
+    for(int i=0;i<tuples;i++) {
+        float sum = 0;
+        for(int j=0;j<attributes;j++) sum += pow((data[i][j] - test[j]), 2);
+        nearDst[i] = sqrt(sum);
+    }
+    cout << "the nearest neighbours are = " <<endl;
+    for(int i=0;i<k;i++) {
+        float mn = 9999;
+        int s = -1;
+        for(int j=0;j<tuples;j++) {
+            if(nearDst[j]<mn) {
+                mn = nearDst[j];
+                s = j;
+            }
+        }
         cout << "(";
-        for (j = 0; j < attr; j++)
-        {
-            cout << " " << data[s][j];
-        }
-        cout << " )" << endl;
+        for(int l=0;l<attributes;l++) cout << data[s][l];
+        cout << ")" <<endl;
+        nearDst[s] = 9999;
     }
-
-    return 0;
-}
-
-void NNC(int d[20][20], int t[1][20])
-{
-    int i, j;
-    float sum;
-
-    for (i = 0; i < tuples; i++)
-    {
-        sum = 0;
-        for (j = 0; j < attr; j++)
-        {
-            sum += (d[i][j] - t[0][j]) * (d[i][j] - t[0][j]);
-        }
-        nearDist[i] = sqrt(sum);
-    }
-}
-
-int isNear()
-{
-    float min = 9999;
-    int s = -1;
-
-    for (int i = 0; i < tuples; i++)
-    {
-        if (nearDist[i] < min)
-        {
-            min = nearDist[i];
-            s = i;
-        }
-    }
-
-    nearDist[s] = 9999;   // mark as used
-    return s;
 }
 
 
